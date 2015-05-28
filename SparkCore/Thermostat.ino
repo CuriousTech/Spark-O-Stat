1a7109af48fdbf94f234e86cec2420ab910b94e5
Greg Cunningham <greg@curioustech.net>
2015-05-28 07:37:50 -0400

    Adding auto heat mode option

--- SparkCore/Thermostat.ino
+++ SparkCore/Thermostat.ino
@@ -24,14 +24,14 @@
 
 //#define T_CAL     // switch to touchscreen calibration mode (draws hits and 4 temp buttons are calibration values adjusted by thumbwheel)
 
-#define DHT_TEMP_ADJUST (-70)  // Adjust indoor temp by -7.0 degress
+#define DHT_TEMP_ADJUST (-100)  // Adjust indoor temp by -7.0 degress
 #define ONE_DAY_MILLIS (24 * 60 * 60 * 1000)
 unsigned long lastSync;
 
 int8_t  tz = -5;         // timezone and DST
 char    ZipCode[] = "41042";
 uint16_t blankSecs = 120;
-int8_t  brightness = 70; // in %
+int8_t  brightness = 60; // in %
 int8_t  dimLevel = 2;    // dim level.  0 = off
 
 void drawButton(int8_t btn, bool bDown, bool bFast);
@@ -79,27 +79,26 @@ static const char _days_short[7][4] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"
 
 void displayTime()
 {
-    char szTime[16];
-    static char lastMin = -1;
+	char szTime[16];
+	static char lastMin = -1;
 
-    sprintf(szTime, "%s %2d:%02d:%02d %cM",
-    	_days_short[Time.weekday()-1], Time.hourFormat12(), Time.minute(), Time.second(), Time.isAM() ? 'A':'P');
+	sprintf(szTime, "%s %2d:%02d:%02d %cM", _days_short[Time.weekday()-1], Time.hourFormat12(), Time.minute(), Time.second(), Time.isAM() ? 'A':'P');
 
-    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-    tft.setTextSize(2);
+	tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+	tft.setTextSize(2);
 
-    if(lastMin == szTime[11]) // draw just seconds
-    {
-        szTime[12] = 0;     // cut to seconds
-    	tft.setCursor(38 + (10*FONT_SPACE), 0);
-    	tft.println(szTime + 10);
-        return;
-    }
+	if(lastMin == szTime[11]) // draw just seconds
+	{
+		szTime[12] = 0;     // cut to seconds
+		tft.setCursor(38 + (10*FONT_SPACE), 0);
+		tft.println(szTime + 10);
+		return;
+	}
 
-    lastMin = szTime[11];
-    if(szTime[4] == '0') szTime[4] = ' ';
+	lastMin = szTime[11];
+	if(szTime[4] == '0') szTime[4] = ' ';
 	tft.setCursor(38, 0);
-    tft.println(szTime);
+	tft.println(szTime);
 }
 
 //---------------------------
@@ -119,50 +118,50 @@ bool bUpdateDST;
 
 void xml_callback(int8_t item, int8_t idx, char *p)
 {
-    int8_t newtz;
-    int8_t h;
-    int8_t d;
-    static int8_t hO;
-    static int8_t lastd;
+	int8_t newtz;
+	int8_t h;
+	int8_t d;
+	static int8_t hO;
+	static int8_t lastd;
 
-    switch(item)
-    {
-        case 0:                             // valid time
-            if(!idx)                        // first item isn't really data.  Use for past data
-            {
-                hO = 0;                     // reset hour offset
-                lastd = Time.day();
-                break;
-            }
-            d = atoi(p + 8);                // 2014-mm-ddThh:00:00-tz:00
-            h = atoi(p + 11);
-            if(d != lastd) hO += 24;        // change to hours offset
-            lastd = d;
-            hvac.m_fcData[idx-1].h = h + hO;
+	switch(item)
+	{
+		case 0:                             // valid time
+			if(!idx)                        // first item isn't really data.  Use for past data
+			{
+				hO = 0;                     // reset hour offset
+				lastd = Time.day();
+				break;
+			}
+			d = atoi(p + 8);                // 2014-mm-ddThh:00:00-tz:00
+			h = atoi(p + 11);
+			if(d != lastd) hO += 24;        // change to hours offset
+			lastd = d;
+			hvac.m_fcData[idx-1].h = h + hO;
 
-            newtz = atoi(p + 20);
-            if(p[19] == '-')
-                newtz = -newtz;
+			newtz = atoi(p + 20);
+			if(p[19] == '-')
+				newtz = -newtz;
 
-            if(idx < 3 && newtz != tz)
-            {
-                tz = newtz;
-                if(idx == 1)
-                {
-                    Time.zone(tz);      // needs change now
-                }
-                else
-                {
-                    bUpdateDST = true;  // change it at 2AM
-                }
-            }
-            break;
-        case 1:                             // temperature
-            if(!idx)                        // 1st value is not temp
-                break;
+			if(idx < 3 && newtz != tz)
+			{
+				tz = newtz;
+				if(idx == 1)
+				{
+					Time.zone(tz);      // needs change now
+				}
+				else
+				{
+					bUpdateDST = true;  // change it at 2AM
+				}
+			}
+			break;
+		case 1:                             // temperature
+			if(!idx)                        // 1st value is not temp
+				break;
 			hvac.m_fcData[idx-1].t = atoi(p);
-            break;
-    }
+			break;
+	}
 }
 
 XMLReader xml(buffer, 257, xml_callback);
@@ -172,20 +171,20 @@ XMLReader xml(buffer, 257, xml_callback);
 //  Other options would be to request different data at different times to split up data that's too large for one time
 void GetForecast()
 {
-    lUpdateFcst = millis();
-
-    Serial.print("Updating forecast at ");
-    Serial.println(Time.timeStr());
-
-    char *p_cstr_array[] =
-    {
-        "/xml/sample_products/browser_interface/ndfdXMLclient.php?zipCodeList=",
-        ZipCode,
-        "&Unit=e&temp=temp&Submit=Submit",
-        NULL
-    };
-    
-    bReading = xml.begin("graphical.weather.gov", p_cstr_array);
+	lUpdateFcst = millis();
+	
+	Serial.print("Updating forecast at ");
+	Serial.println(Time.timeStr());
+	
+	char *p_cstr_array[] =
+	{
+		"/xml/sample_products/browser_interface/ndfdXMLclient.php?zipCodeList=",
+		ZipCode,
+		"&Unit=e&temp=temp&Submit=Submit",
+		NULL
+	};
+	
+	bReading = xml.begin("graphical.weather.gov", p_cstr_array);
 //    if(!bReading)
 //        Serial.println("XMLReader connect failed");
 }
@@ -193,7 +192,7 @@ void GetForecast()
 // get value at current minute between hours
 int tween(int8_t t1, int8_t t2, int m, int8_t h)
 {
-    if(h == 0) h = 1; // div by zero check
+	if(h == 0) h = 1; // div by zero check
 	double t = (double)(t2 - t1) * (m * 100 / (60 * h)) / 100;
 	return (int)((t + t1) * 10);
 }
@@ -232,19 +231,19 @@ void displayOutTemp()
 
 void drawForecast()
 {
-    int8_t min = 126;
-    int8_t max = -50;
-    int8_t i;
+	int8_t min = 126;
+	int8_t max = -50;
+	int8_t i;
 
-    if(hvac.m_fcData[0].h == -1)          // no first run
-        return;
+	if(hvac.m_fcData[0].h == -1)          // no first run
+		return;
 
-    int8_t hrs = ( ((hvac.m_fcData[0].h - Time.hour()) + 1) % 3 ) + 1;   // Set interval to 2, 5, 8, 11..
-    int8_t mins = (60 - Time.minute() + 54) % 60;   // mins to :54, retry will be :59
+	int8_t hrs = ( ((hvac.m_fcData[0].h - Time.hour()) + 1) % 3 ) + 1;   // Set interval to 2, 5, 8, 11..
+	int8_t mins = (60 - Time.minute() + 54) % 60;   // mins to :54, retry will be :59
 
-    if(mins > 10 && hrs > 2) hrs--;     // wrong
+	if(mins > 10 && hrs > 2) hrs--;     // wrong
 
-    FcstInterval = ((hrs * 60) + mins) * 60;
+	FcstInterval = ((hrs * 60) + mins) * 60;
 /*
         Serial.println("drawForecast");
         Serial.print("Hour diff = ");
@@ -272,17 +271,17 @@ void drawForecast()
         Serial.println(fcD[3].t);
 */
 
-    // Get min/max
-    for(i = 0; i < 18; i++)
-    {
-        int8_t t = hvac.m_fcData[i].t;
-        if(min > t) min = t;
-        if(max < t) max = t;
-    }
-
-    if(min == max) max++;   // div by 0 check
-
-    hvac.updatePeaks(min, max);
+	// Get min/max
+	for(i = 0; i < 18; i++)
+	{
+		int8_t t = hvac.m_fcData[i].t;
+		if(min > t) min = t;
+		if(max < t) max = t;
+	}
+	
+	if(min == max) max++;   // div by 0 check
+	
+	hvac.updatePeaks(min, max);
 
 	int16_t y = Fc_Top;
 	int16_t incy = (Fc_Bottom - Fc_Top) / 3;
@@ -290,10 +289,10 @@ void drawForecast()
 	int16_t t = max;
 	int16_t x;
 
-    tft.setTextSize(1);
+	tft.setTextSize(1);
 	tft.setTextColor( ILI9341_YELLOW, ILI9341_BLACK );
 
-    tft.fillRect(0, Fc_Top, Fc_Right, Fc_Bottom-Fc_Top+FONT_Y+1, ILI9341_BLACK);
+	tft.fillRect(0, Fc_Top, Fc_Right, Fc_Bottom-Fc_Top+FONT_Y+1, ILI9341_BLACK);
 
     // temp scale and h-lines
 	for(i = 0; i <= 3; i++)
@@ -315,22 +314,22 @@ void drawForecast()
 
 	int8_t day = Time.weekday()-1;              // current day
 	int8_t h0 = Time.hour();                    // zeroeth hour
-    int8_t pts = hvac.m_fcData[17].h - h0;
-    int8_t h;
-    int16_t day_x = 0;
-
-    if(pts <= 0) return;                     // error
+	int8_t pts = hvac.m_fcData[17].h - h0;
+	int8_t h;
+	int16_t day_x = 0;
+	
+	if(pts <= 0) return;                     // error
 
 	for(i = 0, h = h0; i < pts; i++, h++)    // v-lines
 	{
-        x = Fc_Left + (Fc_Right-Fc_Left) * i / pts;
+		x = Fc_Left + (Fc_Right-Fc_Left) * i / pts;
 		if( (h % 24) == 0) // midnight
 		{
 			tft.drawFastVLine(x, Fc_Top, Fc_Bottom - Fc_Top, GRAY1);
 			if(x - 49 > Fc_Left) // fix 1st day too far left
 			{
 				tft.setCursor(day_x = x - 49, Fc_Bottom + 2);
-			    tft.print(_days_short[day]);
+				tft.print(_days_short[day]);
 			}
 			if(++day >6) day = 0;
 		}
@@ -402,6 +401,7 @@ int8_t  dispNote = -1;
 
 const char  *pszHvacFan[] = {"Auto", " On "};
 const char  *pszHvacMode[] = {"Off ", "Cool", "Heat", "Auto"};
+const char  *pszHeatMode[] = {" HP ", " NG ", "Auto"};
 
 // convert n*10 to XXX.X with neg
 void cvtNum(char *p, int num)
@@ -528,15 +528,15 @@ void drawButton(int8_t btn, bool bDown, bool bFast) // draw a button with presse
             szText[6] = 0;
             break;
         case 8: // fan
-            pszText = (char *)pszHvacFan[hvac.getFan()];
+            pszText = (char *)pszHvacFan[ hvac.getFan() ];
             bg = bDown ? ILI9341_RED:ILI9341_BLUE;
             break;
         case 9: // mode
-            pszText = (char *)pszHvacMode[hvac.getSetMode()];
+            pszText = (char *)pszHvacMode[ hvac.getSetMode() ];
             bg = bDown ? ILI9341_RED:ILI9341_BLUE;
             break;
-        case 10:
-            pszText = (char *)(hvac.getHeatMode() ? "NG" : "HP");
+        case 10: // heat mode
+            pszText = (char *)pszHeatMode[ hvac.getHeatMode() ];
             bg = bDown ? ILI9341_RED:ILI9341_BLUE;
             break;
         case 11: // Notifications
@@ -599,30 +599,30 @@ void Backlight(uint8_t bright)
 
 void HandleTouch(bool bDown)
 {
-    static int8_t lastButton = -1;
-    static int16_t lastX = -1, lastY = -1;
-    static bool bPress = false;
-
-    lBacklightTimer = millis();     // reset screensaver
-
-    if(!bScreenOn)
-    {
-        Backlight(brightness);
-        bScreenOn = true;
-        lastButton = -1;
-        return;     // block first press if screen was off
-    }
-
-    if( !bDown )      // button release
-    {
-        drawButton(lastButton, false, false);
-        bPress = false;
-        return;
-    }
-
+	static int8_t lastButton = -1;
+	static int16_t lastX = -1, lastY = -1;
+	static bool bPress = false;
+	
+	lBacklightTimer = millis();     // reset screensaver
+	
+	if(!bScreenOn)
+	{
+		Backlight(brightness);
+		bScreenOn = true;
+		lastButton = -1;
+		return;     // block first press if screen was off
+	}
+	
+	if( !bDown )      // button release
+	{
+		drawButton(lastButton, false, false);
+		bPress = false;
+		return;
+	}
+	
 	touch.read();
-    int16_t x = touch.getX();
-    int16_t y = touch.getY();
+	int16_t x = touch.getX();
+	int16_t y = touch.getY();
 
 #ifdef T_CAL // display for calibration
         Serial.print("Touch TP_X=");
@@ -636,9 +636,9 @@ void HandleTouch(bool bDown)
 
         if(lastX != -1)
         {
-            tft.drawFastHLine(lastX-20, lastY, 40, ILI9341_BLACK);
-            tft.drawFastVLine(lastX, lastY-20, 40, ILI9341_BLACK);
-        	tft.drawCircle(lastX, lastY, 15, ILI9341_BLACK);
+		tft.drawFastHLine(lastX-20, lastY, 40, ILI9341_BLACK);
+		tft.drawFastVLine(lastX, lastY-20, 40, ILI9341_BLACK);
+		tft.drawCircle(lastX, lastY, 15, ILI9341_BLACK);
         }
         tft.drawFastHLine(x-20, y, 40, ILI9341_CYAN);
         tft.drawFastVLine(x, y-20, 40, ILI9341_CYAN);
@@ -680,13 +680,13 @@ void HandleTouch(bool bDown)
         case 8: // Fan auto, on
             hvac.setFan( !hvac.getFan() );
             break;
-        case 9: // off, Cool, heat, e-heat, auto
+        case 9: // off, Cool, heat, auto
             hvac.setMode( (hvac.getSetMode() + 1) & 3 );
             drawButton(1, false, false);  // remove prev highlight and display current highlight
             drawButton(2, false, false);
             break;
         case 10: // heat mode
-            hvac.setHeatMode( !hvac.getHeatMode() );
+            hvac.setHeatMode( (hvac.getHeatMode() + 1) % 3 );
             drawButton(9, false, false);
             break;
         case 11: // Notifications
@@ -737,16 +737,25 @@ bool EncoderCheck()
 void DrawButtons()
 {
     const char szLabels[3][4] = {"Out", "In", "Rh"};
+    const char szLabels2[3][6] = {"Fan", "Mode", "Heat"};
+    int8_t i;
 
     tft.setTextColor(ILI9341_YELLOW, ILI9341_BLACK);
     tft.setTextSize(1);
-    for(int i = 0; i < 3; i++)
+    for(i = 0; i < 3; i++)
     {
         tft.setCursor(buttons[i+5].x - (FONT_SPACE*3), buttons[i+5].y + 4);
     	tft.print((char *)szLabels[i]);
     }
 
-    for(int8_t i = 0; buttons[i].w; i++)
+    tft.setTextSize(2);
+    for(i = 0; i < 3; i++)
+    {
+        tft.setCursor(buttons[i+8].x + buttons[i+8].w + 10, buttons[i+8].y + 8);
+    	tft.print((char *)szLabels2[i]);
+    }
+
+    for(i = 0; buttons[i].w; i++)
     {
         drawButton(i, false, false);
     }
@@ -851,42 +860,44 @@ void ReadEE()   // read EEPROM on startup
         p[i] = EEPROM.read(i);
     }
     hvac.setMode(hvac.getMode()); // set request mode to EE mode
+    hvac.setHeatMode(hvac.getHeatMode());
 }
 
 //------------------------------
 void setup()
 {
-    Spark.function("setvar", sf_setVar);
-    Spark.function("getvar", sf_getVar);
-    Spark.variable("result", hvac.m_szResult, STRING);
-    Serial.begin(9600);
-
-    ReadEE();
-
-    tft.begin();
-    tft.setRotation(3);
+	Spark.function("setvar", sf_setVar);
+	Spark.function("getvar", sf_getVar);
+	Spark.variable("result", hvac.m_szResult, STRING);
+	Serial.begin(9600);
+	
+	ReadEE();
+	
+	tft.begin();
+	tft.setRotation(3);
 	tft.fillScreen(ILI9341_BLACK);
-
-    tft.setFont(CENTURY_8);
+	
+	tft.setFont(CENTURY_8);
 
 	pinMode(A6, OUTPUT);
 	Backlight(brightness);
 
-    //                h,   w,  top, btm, lft, rght, 
-    touch.InitTouch(240, 320, 2000, 210, 250, 1860, LANDSCAPE);
-
-    lUpdateDHT = millis();
-    Time.zone(tz);
-
+	//                h,   w,  top, btm, lft, rght, 
+	touch.InitTouch(240, 320, 2000, 210, 250, 1860, LANDSCAPE);
+	
+	lUpdateDHT = millis();
+	Time.zone(tz);
+	
 	lastSync = millis();
-    lUpdateFcst = millis();
-    lBacklightTimer = millis();
-
-    FcstInterval = 15; // first time = 30 seconds
-    DrawButtons();
-
+	lUpdateFcst = millis();
+	lBacklightTimer = millis();
+	
+	FcstInterval = 15; // first time = 30 seconds
+	DrawButtons();
+	
 	DHT.acquire();
-    bDHT = true;
+	bDHT = true;
+	hvac.enable();
 }
 
 void loop()
@@ -895,7 +906,6 @@ void loop()
     static int8_t lastHour;
     static int8_t lastMode;
     static bool bDown = false;
-    static bool bRun = false;
 
     if( touch.pressed() )
     {
@@ -938,8 +948,8 @@ void loop()
                 tone(D1, 3200, 150);
             }
 
-	    }
-	
+	}
+
         displayTime();
         hvac.service();
 
@@ -971,8 +981,8 @@ void loop()
         }
         else if(millis() - lUpdateDHT >= DHT_PERIOD)
         {
-        	DHT.acquire();
-            bDHT = true;
+		DHT.acquire();
+		bDHT = true;
         }
 
         if(bReading)
